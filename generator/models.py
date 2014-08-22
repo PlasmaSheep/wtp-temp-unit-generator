0543432998e2ffeb283321019b260e065bc8be81
PlasmaSheep <plasmasheep@gmail.com>
2014-08-22 09:00:17 -0700

    Wrote models and unit tests

--- generator/models.py
+++ generator/models.py
@@ -25,10 +25,33 @@ class Class(object):
 
     def add_student(self, student, unit):
         if not self.student_exists(student):
-            student.unit_mates.extend(self.units[unit])
+            student.unit_mates.extend(self.units[unit].students)
             self.units[unit].append(student)
         else:
             raise ValueError("Student exists already in this class.")
+
+class Unit(object):
+    """A unit contains several students.
+    """
+    def __init__(self, max_students):
+        self.max_students = max_students
+        self.students = []
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == len(self.students):
+            raise StopIteration
+        self.index += 1
+        return self.students[self.index - 1]
+
+    def append(self, item):
+        if len(self.students) < self.max_students:
+            self.students.append(item)
+        else:
+            raise ValueError("Too many students in this unit.")
 
 class Student(object):
     """Represents a student in a class and in units.
--- tests/test_models.py
+++ tests/test_models.py
@@ -4,46 +4,54 @@ import unittest
 
 from generator.models import Class
 from generator.models import Student
+from generator.models import Unit
 
 class TestClass(unittest.TestCase):
     """Tests for the Class model.
     """
     def setUp(self):
         self.class_object = Class()
+        unit1 = Unit(2)
+        unit2 = Unit(2)
+        unit1.students = [Student("a"), Student("b")]
+        unit2.students = [Student("c")]
+        self.class_object.units = [unit1, unit2]
 
     def test_student_exists(self):
         """Check student_exists.
         """
-        self.class_object.units = [
-            [Student("a"), Student("b")],
-            [Student("c")]]
-
         assert self.class_object.student_exists(Student("c"))
         assert not self.class_object.student_exists(Student("f"))
 
     def test_add_existing_student(self):
         """See if we can add an existing student.
         """
-        self.class_object.units = [
-            [Student("a"), Student("b")],
-            [Student("c")]]
-
         self.assertRaises(ValueError, self.class_object.add_student,
             Student("a"), 1)
 
     def test_add_new_student(self):
         """See if we can add a nonexistant student.
         """
-        self.class_object.units = [
-            [Student("a"), Student("b")],
-            [Student("c")]]
-
         new_student = Student("foo")
 
         self.class_object.add_student(new_student, 1)
 
         assert new_student in self.class_object.units[1]
         assert new_student.unit_mates == [Student("c")]
+
+class TestUnit(unittest.TestCase):
+    """Tests for the Unit model.
+    """
+    def setUp(self):
+        self.unit = Unit(5)
+
+    def test_append(self):
+        """Make sure there's a limit to how much we can append
+        """
+        for i in range(0, self.unit.max_students):
+            self.unit.append(i)
+
+        self.assertRaises(ValueError, self.unit.append, 2)
 
 class TestStudent(unittest.TestCase):
     """Tests for the Student model.
