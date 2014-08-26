5b43e8e83ab46a38fc082dab1fe33ea3fb47dfd0
PlasmaSheep <plasmasheep@gmail.com>
2014-08-26 09:35:54 -0700

    Generalized code to arbitrary numbers of units

--- generator/generator.py
+++ generator/generator.py
@@ -5,18 +5,18 @@ from .models import Class
 from .models import Unit
 from .models import Student
 
-def get_unit_sizes(class_size):
+def get_unit_sizes(class_size, number_of_units):
     """Given a class size, determine the best unit sizing.
 
     Arguments:
         class_size (int): Number of students in the class.
     """
 
-    base_size = int(class_size / 6)
-    remainder = class_size - base_size * 6
+    base_size = int(class_size / number_of_units)
+    remainder = class_size - base_size * number_of_units
     unit_sizes = []
 
-    for i in range(0, 6):
+    for i in range(0, number_of_units):
         if i < remainder:
             unit_sizes.append(base_size + 1)
         else:
@@ -29,7 +29,7 @@ def get_unit_sizes(class_size):
 class Generator(object):
     """The temp unit generator class.
     """
-    def __init__(self, class_size, iterations, student_names):
+    def __init__(self, iterations, student_names, number_of_units):
         """Instantiate a temp unit generator.
 
         Arguments:
@@ -38,10 +38,10 @@ class Generator(object):
             student_names (list of str): Names of the students.
         """
 
-        self.class_size = class_size
+        self.class_size = len(student_names)
         self.students = []
         self.classes = []
-        unit_sizes = get_unit_sizes(class_size)
+        unit_sizes = get_unit_sizes(self.class_size, number_of_units)
 
         for i in range(0, iterations):
             new_class = Class()
